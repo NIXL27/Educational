647af59526c7c4e9821ed9e720694c8b311d54b3
lx <2904937506@qq.com>
2022-05-07 17:44:42 +0800

    合并冲突

--- src/main/java/com/fc/service/impl/SelectedcourseServiceImpl.java
+++ src/main/java/com/fc/service/impl/SelectedcourseServiceImpl.java
@@ -1,20 +1,23 @@
 package com.fc.service.impl;
 
+import com.fc.entity.SelectedcourseExample;
 import com.fc.dao.SelectedcourseMapper;
 import com.fc.entity.Selectedcourse;
-import com.fc.entity.SelectedcourseExample;
 import com.fc.service.SelectedcourseService;
-import com.fc.vo.SelectedcourseVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class SelectedcourseServiceImpl implements SelectedcourseService {
+public class SelectedCourseServiceImpl implements SelectedcourseService {
     @Autowired
-    SelectedcourseMapper selectedcourseMapper;
+    private SelectedcourseMapper selectedcourseMapper;
+
 
     @Override
     public void update(Integer studentid, Integer courseid, String name, Integer mark) {
+
         SelectedcourseExample selectedcourseExample = new SelectedcourseExample();
         Selectedcourse selectedcourse = new Selectedcourse();
         selectedcourse.setCourseid(courseid);
@@ -24,5 +27,11 @@ public class SelectedcourseServiceImpl implements SelectedcourseService {
         criteria.andCourseidEqualTo(courseid);
         criteria.andStudentidEqualTo(studentid);
         selectedcourseMapper.updateByExampleSelective(selectedcourse,selectedcourseExample);
+
+    }
+
+    @Override
+    public List<Selectedcourse> findStudentByMark(Integer id) {
+        return selectedcourseMapper.findStudentByMark(id);
     }
 }
